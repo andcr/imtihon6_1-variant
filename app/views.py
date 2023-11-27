f4ee888b119504f98b54324116bcc9ef30da9407
andcr <fatxullohibrohim@gmail.com>
2023-11-27 19:44:25 +0500

    fdaxds

--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Content
 
-# Create your views here.
+def content_list(request):
+    contents = Content.objects.all()
+    return render(request, 'content/content_list.html', {'contents': contents})
